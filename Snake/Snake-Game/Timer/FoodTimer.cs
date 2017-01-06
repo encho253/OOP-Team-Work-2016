59646374d1318107e44de6d3c6bbdcbdc1b694ea
EvgeniTrifonov <trifonov.e.v@gmail.com>
2017-01-05 18:43:22 -0800

    Update class FoodTimer.

--- Snake/Snake-Game/Timer/FoodTimer.cs
+++ Snake/Snake-Game/Timer/FoodTimer.cs
@@ -13,32 +13,21 @@ namespace Snake_Game.Timer
     using AbstractClasses;
     class FoodTimer:Food
     {
-        public static int lastTimeFood { get; set; }
-        public static int lastTimeBigFood { get; set; }
         public static int NewFood(Food food, int lastTimeFood, int foodDissapearTime)
-        {            
+        {
             if (Environment.TickCount - lastTimeFood >= foodDissapearTime)
             {
-                
+
                 Console.SetCursorPosition(food.Position.Col, food.Position.Row);
                 Console.Write(" ");
-                Thread.Sleep(50);                
+                Thread.Sleep(50);
                 var egg = NewPosition();
-                food.Position= egg;                
-                food.DrawingFood();               
-                if (food.Name == "o")
-                {
-                    FoodTimer.lastTimeFood = Environment.TickCount;
-                    lastTimeFood = FoodTimer.lastTimeFood;
-                }
-                else if (food.Name == "O")
-                {
-                    FoodTimer.lastTimeBigFood = Environment.TickCount;
-                    lastTimeFood = FoodTimer.lastTimeBigFood;                  
-                }
+                food.Position = egg;
+                food.DrawingFood();
+                lastTimeFood = Environment.TickCount;
             }
             return lastTimeFood;
         }
-        
+
     }
 }
