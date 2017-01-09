08b0118211dd459cb91a50d6d3245de8b6475987
encho253 <encho.enevski@gmail.com>
2017-01-09 20:29:13 +0200

    Snake Head
    Added new classes SnakeHead and SnakeTail.

---
+++ Snake/Snake-Game/SnakeElements/SnakeHead.cs
@@ -0,0 +1,27 @@
+﻿namespace Snake_Game.SnakeBody
+{
+    using Snake_Game.Struct;
+
+    public class SnakeHead
+    {     
+        public const char Symbol= '@';
+        private Position head;
+
+        public SnakeHead(int tailSize)
+        {
+            this.head = new Position(tailSize, 0);
+        }
+
+        public Position Head
+        {
+            get
+            {
+                return this.head;
+            }
+            set
+            {
+                this.head = value;
+            }
+        }
+    }
+}
---
+++ Snake/Snake-Game/SnakeElements/SnakeTail.cs
@@ -0,0 +1,31 @@
+﻿namespace Snake_Game.SnakeElements
+{
+    using Snake_Game.Struct;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SnakeTail
+    {
+        public const int InitialTailSize = 5;
+        public const char ElementSymbol = '*';
+
+        public SnakeTail(int tailSize)
+        {
+            this.TailSize = tailSize;        
+        }
+
+        public Queue<Position> TailElements { get; set; }       
+
+        public int TailSize { get; set; }
+
+        public SnakeTail Tail { get; set; }
+
+        public Position Neck
+        {
+            get
+            {
+                return this.TailElements.Last();
+            }
+        }
+    }
+}
